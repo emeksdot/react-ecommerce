010e739cfd1a37faa960d783e853e7d45a208d35
emeksdot <emeksdot@yahoo.com>
2021-12-02 00:31:13 +0100

    Cleaned up the Home page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <h1>Emmanuel's Page</h1>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
       <Navbar />
     </div>
   );
